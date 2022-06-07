759176b74b1615227847327355988e572effcbd4
Prerit Vishal <preritvishal3@gmail.com>
2022-06-07 09:38:50 +0530

    Updated common js and css files for connecting with newer pages

--- js/page-common.js
+++ js/page-common.js
@@ -4,5 +4,13 @@
 //      regex to validate page name: "page\d+.xhtml"
 
 function goToPage(num){
-    window.location.href = "page" + num + ".html";
+    window.location.href = "page" + num + ".xhtml";
+}
+
+function correct() {
+    alert("Correct Answer !!");
+}
+
+function incorrect() {
+    alert("Wrong Answer !!")
 }
