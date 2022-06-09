0f85adf492dc2b04c102b7e28750efb3aba8aa30
Prerit Vishal <preritvishal3@gmail.com>
2022-06-09 07:49:51 +0530

    Commented all the js code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
-let firstName = null;
-let lastName = null;
-let age = null;
-let gender = null;
-let email = null;
+// i've no idea how it works, but okay
+// lets give it a try
 
-function saveInfo() {
-    firstName = document.getElementById('user-first-name').innerText;
-    lasttName = document.getElementById('user-last-name').innerText;
-    age = int(document.getElementById('user-age').innerText);
-    gender = document.getElementById('user-gender').innerText;
-    email = document.getElementById('user-email').innerText;
+// let firstName = null;
+// let lastName = null;
+// let age = null;
+// let gender = null;
+// let email = null;
 
-    alert(firstName + " " + lastName);
-    alert(age);
-    alert(gender);
-    alert(email);
-}
+// function saveInfo() {
+//     firstName = document.getElementById('user-first-name').innerText;
+//     lasttName = document.getElementById('user-last-name').innerText;
+//     age = int(document.getElementById('user-age').innerText);
+//     gender = document.getElementById('user-gender').innerText;
+//     email = document.getElementById('user-email').innerText;
+
+//     alert(firstName + " " + lastName);
+//     alert(age);
+//     alert(gender);
+//     alert(email);
+// }
