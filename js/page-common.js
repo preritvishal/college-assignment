12cf0b7f6436d5fc84cd33086c56260484ec0f9e
Prerit Vishal <preritvishal3@gmail.com>
2022-06-11 13:42:33 +0530

    Now stores score of user on session basis

--- js/page-common.js
+++ js/page-common.js
@@ -2,8 +2,19 @@
 //      to the page number provided
 //      page number must be in format
 //      regex to validate page name: "page\d+.xhtml"
-function goToPage(num){
-    window.location.href = "page" + num + ".xhtml";
+var correctAnswer = parseInt(sessionStorage.getItem("correctAnswer")) || 0;
+var incorrectAnswer = parseInt(sessionStorage.getItem("incorrectAnswer")) || 0;
+var unanswered = parseInt(sessionStorage.getItem("unanswered")) || 0;
+
+// method to save the score to the session storage
+function saveScore() {
+    sessionStorage.setItem("correctAnswer",  correctAnswer);
+    sessionStorage.setItem("incorrectAnswer", incorrectAnswer);
+    sessionStorage.setItem("unanswered", unanswered);
+}
+
+function goToPage(num) {
+    window.location.href = "page" + num + ".html";
 }
 
 // function to get read this page name
@@ -22,17 +33,60 @@ function getPageNumber(pageName) {
 
 // functnion to run when input is correct
 function correct() {
-    alert("Correct Answer !!");
-    goToPage(parseInt(getPageNumber(getPageName()) + 1));
+    // increment correct answer
+    correctAnswer += 1;
+    saveScore();
+    nextPage(parseInt(getPageNumber(getPageName()) + 1));
 }
 
 // function to check if input is correct
 function incorrect() {
-    alert("Wrong Answer !!")
-    goToPage(parseInt(getPageNumber(getPageName()) + 1));
+    // alert("Wrong Answer !!")
+    incorrectAnswer += 1;
+    saveScore();
+    nextPage(parseInt(getPageNumber(getPageName()) + 1));
 }
 
 // function to skip page
 function skipPage() {
-    goToPage(parseInt(getPageNumber(getPageName()) + 1));
+    unanswered += 1;
+    saveScore();
+    nextPage(parseInt(getPageNumber(getPageName()) + 1));
+}
+
+function nextPage(num) {
+    if (num > 10) {
+        getScore();
+        location.href = "./score.html";
+    }
+    else {
+        goToPage(num);
+    }
+}
+
+// function to rickroll anyone
+function rickroll() {
+    let body = document.getElementById("body");
+    body.innerHTML = "<iframe src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+// function to go back to first page
+function goBack() {
+    window.location.href = "../index.html";
+}
+
+// inject the score to the score page
+function getScore() {
+    let para = document.getElementById('question-element-display');
+    getLocalScore();
+    para.innerHTML = "Correct Answer: " + correctAnswer + "&ThickSpace; Incorrect Answer: " + incorrectAnswer + "&ThickSpace; Unanswered: " + unanswered;
+}
+
+function textMe() {
+    window.location.href="https://wa.me/+918407836969?text=Hi%20There";
+}
+
+function firstPage() {
+    window.location.href = "./src/page1.html";
 }
